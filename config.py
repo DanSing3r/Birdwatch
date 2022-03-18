a920e67026fae206d85e91bbb49965a6bad7f9a0
TiptoesJohnson <desinger@protonmail.com>
2022-03-17 21:45:26 -0500

    Added county names

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 '''Collin, Dallas, Denton, Ellis, Hunt, Kaufman, Rockwall, Johnson, Parker
 Tarrant, Wise'''
 
-REGIONS = ['US-TX-113', 'US-TX-121', 'US-TX-439', 'US-TX-085', 'US-TX-397',
-    'US-TX-139', 'US-TX-231', 'US-TX-257', 'US-TX-251', 'US-TX-367', 'US-TX-497']
+REGIONS = [('Dallas', 'US-TX-113'), ('Denton', 'US-TX-121'), ('Tarrant', 'US-TX-439'), ('Collin', 'US-TX-085'), ('Rockwall', 'US-TX-397'),
+    ('Ellis', 'US-TX-139'), ('Hunt', 'US-TX-231'), ('Kaufman', 'US-TX-257'), ('Johnson', 'US-TX-251'), ('Parker', 'US-TX-367'), ('Wise', 'US-TX-497')]
 TIMEFRAME=7
 F_TWEETED='tweeted.json'
 DELAY=120
