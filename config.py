bc7d67dcbecdc580ba4b1d524c5b118357369999
TiptoesJohnson <desinger@protonmail.com>
2022-03-30 21:49:28 -0500

    Added birds to ignore

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-'''Collin, Dallas, Denton, Ellis, Hunt, Kaufman, Rockwall, Johnson, Parker
-Tarrant, Wise'''
-
 REGIONS = [('Dallas', 'US-TX-113'), ('Denton', 'US-TX-121'), ('Tarrant', 'US-TX-439'), ('Collin', 'US-TX-085'), ('Rockwall', 'US-TX-397'),
     ('Ellis', 'US-TX-139'), ('Hunt', 'US-TX-231'), ('Kaufman', 'US-TX-257'), ('Johnson', 'US-TX-251'), ('Parker', 'US-TX-367'), ('Wise', 'US-TX-497')]
+IGNORE=['naswar', 'fiscro']
 TIMEFRAME=7
-F_TWEETED='tweeted.json'
 DELAY=120
+F_TWEETED='tweeted.json'
