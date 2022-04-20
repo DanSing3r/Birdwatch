e4c60305017416d26c8f6c9281249b5986c3f976
TiptoesJohnson <desinger@protonmail.com>
2022-04-20 11:34:39 -0500

    Added header to link verification

--- Birdwatch.py
+++ Birdwatch.py
@@ -77,10 +77,16 @@ def tweet(birds, interval=config.DELAY):
         else:
             group_detail = ''
 
+        print(f'name = {bird["comName"]}')
         name = cleanup(bird['comName']).replace(' ', '_').replace('\'', '')
+        print(f'cleaned = {name}')
         about = f'https://allaboutbirds.org/guide/{name}/'
+        print(f'link = {about}')
 
-        b_response = requests.get(about)
+        headers = {'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) '
+            'Gecko/20100101 Firefox/99.0'}
+        b_response = requests.get(about, headers=headers)
         print(b_response.status_code)
         print(type(b_response.status_code))
         if b_response.status_code != 200:
